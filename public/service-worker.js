bb85856a386876577047a85e75503c9966856ef2
Ben <liaoguijunchn@gmail.com>
2022-12-20 17:09:28 +0800

    feat: add eg

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,13 @@
+// sw.js
 
+// 监听 'install' 事件， 回调中缓存所需要的文件
 self.addEventListener('install', function(event) {
   console.log('service worker, install', event)
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll(['./index.html', './index.js'])
+    })
+    )
 });
 
 self.addEventListener('activate', function(event) {
@@ -8,11 +15,21 @@ self.addEventListener('activate', function(event) {
 });
 
 
+// 拦截所有请求事件
+// 如果缓存中已经存在已经有请求的数据就直接用缓存，否则去请求数据
 self.addEventListener('fetch', function(event) {
   console.log('service worker, fetch', event)
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if(response) {
+        return response
+      }
+      console.log('fetch source')
+    })
+  )
 });
 
-self.addEventListener('message', function(event) {
-  console.log('service worker, message', event)
-  event.source.postMessage('this message is from sw.js to page');  // 向主线程传递信息
-});
+// self.addEventListener('message', function(event) {
+//   console.log('service worker, message', event)
+//   event.source.postMessage('this message is from sw.js to page');  // 向主线程传递信息
+// });
